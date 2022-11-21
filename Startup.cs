5a71cb749215505e7fa0769447578d5e011ef0cc
user25 <user25@user25.ru>
2022-11-21 13:29:38 +0500

    Делаем документацию по таблице Gender.

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,8 @@ namespace BeautySaloonAPI
             services.AddControllers();
             services.AddSwaggerGen();
 
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling =Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
